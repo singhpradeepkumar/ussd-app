366f55dddf1aada472a804eb52ee7458f850ad2f
singhpradeepkumar <singhpradeepkumar@hotmail.com>
2019-04-07 21:38:38 +0530

    Update UssdController.java

--- src/main/java/com/ussd/controller/UssdController.java
+++ src/main/java/com/ussd/controller/UssdController.java
@@ -4,6 +4,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.africastalking.AfricasTalking;
+import com.ussd.config.UssdConfig;
+
 @RestController(value="/")
 public class UssdController {
 	
